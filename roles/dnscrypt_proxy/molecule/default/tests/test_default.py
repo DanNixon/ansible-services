e00bd60465c83ae6c5c1bb67f258ea20e7235be8
Dan Nixon <dan@dan-nixon.com>
2021-06-12 16:01:14 +0100

    Tidy dnscrypt_proxy tests

--- roles/dnscrypt_proxy/molecule/default/tests/test_default.py
+++ roles/dnscrypt_proxy/molecule/default/tests/test_default.py
@@ -6,34 +6,24 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-@pytest.mark.parametrize("package_name", [
-    "dnscrypt-proxy",
-])
-def test_packages(host, package_name):
-    assert host.package(package_name).is_installed
+def test_packages(host):
+    assert host.package("dnscrypt-proxy").is_installed
 
 
 def test_config_file(host):
     f = host.file("/etc/dnscrypt-proxy/dnscrypt-proxy.toml")
     assert f.is_file
-    assert f.user == "root"
-    assert f.group == "root"
-    assert f.mode == 0o644
+    assert f.contains("require_dnssec = true")
 
 
 def test_blocked_names(host):
     f = host.file("/etc/dnscrypt-proxy/blocked-names.txt")
     assert f.is_file
-    assert f.user == "root"
-    assert f.group == "root"
-    assert f.mode == 0o644
-    # TODO
+    assert f.contains("google.com")
+    assert f.contains("facebook.com")
 
 
 def test_forwarding_rules(host):
     f = host.file("/etc/dnscrypt-proxy/forwarding-rules.txt")
     assert f.is_file
-    assert f.user == "root"
-    assert f.group == "root"
-    assert f.mode == 0o644
-    # TODO
+    assert f.contains("lan 192.168.1.1")
